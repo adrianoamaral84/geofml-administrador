a28696168d1faec1a98e50adc13d96bb0568d73e
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 15:35:13 -0300

    Corrige redirecionamento apos redefinicao de senha

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Rules\StrongPassword;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
     use ResetsPasswords;
 
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/login';
 
     protected function rules()
     {
@@ -20,5 +22,19 @@ class ResetPasswordController extends Controller
             'email' => 'required|email',
             'password' => ['required', 'confirmed', new StrongPassword()],
         ];
+    }
+
+    protected function resetPassword($user, $password)
+    {
+        $user->password = Hash::make($password);
+        $user->setRememberToken(Str::random(60));
+        $user->save();
+
+        event(new PasswordReset($user));
+    }
+
+    protected function sendResetResponse($request, $response)
+    {
+        return redirect('/login')->with('status', 'Senha alterada com sucesso. Entre novamente com a sua nova senha.');
     }
 }
