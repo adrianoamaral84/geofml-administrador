bcca48b344930bb4628f4e5f6653c20c50bae888
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 13:58:16 -0300

    Ajusta sessao administrativa para 60 minutos

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 60),
 
     'expire_on_close' => false,
 
@@ -147,7 +147,7 @@ return [
     | Session Cookie Domain
     |--------------------------------------------------------------------------
     |
-    | Here you may change the domain of the cookie used to identify a session
+    | Here you may change the domain of the cookie used to identify the session
     | in your application. This will determine which domains the cookie is
     | available to in your application. A sensible default has been set.
     |
@@ -161,8 +161,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | By setting this option to true, session cookies will only be sent back
-    | to the server if the browser has a HTTPS connection. This will keep
-    | the cookie from being sent to you if it can not be done securely.
+    | to the server if the browser has a HTTPS connection. This will keep the
+    | cookie from being sent to you if it can not be done securely.
     |
     */
 
