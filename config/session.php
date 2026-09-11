61f1972dfdc7057d3618e81624cfc8397f448172
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 14:00:39 -0300

    Limpa diff da configuracao de sessao

--- config/session.php
+++ config/session.php
@@ -79,9 +79,9 @@ return [
     | Session Database Table
     |--------------------------------------------------------------------------
     |
-    | When using the "database" session driver, you may specify the table we
-    | should use to manage the sessions. Of course, a sensible default is
-    | provided for you; however, you are free to change this as needed.
+    | When using the "database" or "redis" session drivers, you may specify the
+    | connection that should be used to manage these sessions. This value
+    | should correspond with a connection defined in your database config.
     |
     */
 
@@ -147,7 +147,7 @@ return [
     | Session Cookie Domain
     |--------------------------------------------------------------------------
     |
-    | Here you may change the domain of the cookie used to identify the session
+    | Here you may change the domain of the cookie used to identify a session
     | in your application. This will determine which domains the cookie is
     | available to in your application. A sensible default has been set.
     |
