55b20f12b622b2e218627a08712db8e8b7105473
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 15:34:53 -0300

    Traduz email de redefinicao de senha para pt-BR

---
+++ app/Notifications/ResetPasswordPtBr.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordPtBr extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = url(route('password.reset', $this->token, false).'?email='.urlencode($notifiable->email));
+        $expire = config('auth.passwords.users.expire', 60);
+
+        return (new MailMessage)
+            ->subject('Redefinição de senha - GEOFML')
+            ->greeting('Olá!')
+            ->line('Você está recebendo este e-mail porque foi solicitada uma redefinição de senha para a sua conta.')
+            ->action('Redefinir senha', $url)
+            ->line("Este link de redefinição de senha expira em {$expire} minutos.")
+            ->line('Se você não solicitou a redefinição de senha, nenhuma ação é necessária.')
+            ->salutation('Atenciosamente, Administração do Sistema GEOFML');
+    }
+}
